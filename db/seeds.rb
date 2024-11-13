36248c786e938a850e17bbeadee73cff4b67e991
loris morlais <loris.morlais@icloud.com>
2024-11-13 17:24:50 +0900

    airfirend done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+Flat.destroy_all
+
+apartment_images = [
+  "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267",
+  "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688",
+  "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2",
+  "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?q=80&w=3901&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1512917774080-9991f1c4c750",
+  "https://images.unsplash.com/photo-1554995207-c18c203602cb",
+  "https://images.unsplash.com/photo-1536376072261-38c75010e6c9",
+  "https://images.unsplash.com/photo-1505691938895-1758d7feb511",
+  "https://images.unsplash.com/photo-1574362848149-11496d93a7c7",
+  "https://images.unsplash.com/photo-1560184897-ae75f418493e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1448630360428-65456885c650?q=80&w=3867&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1484154218962-a197022b5858?q=80&w=3874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+]
+
+apartment_images.shuffle!.each_with_index do |image_url, index|
+  Flat.create!(
+    name: Faker::Sports::Football.player,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.most_interesting_man_in_the_world,
+    price_per_night: Faker::Number.between(from: 50, to: 500),
+    number_of_guests: Faker::Number.between(from: 2, to: 10),
+    image_url: image_url
+  )
+end
+
+puts "Created #{Flat.count} flats"
